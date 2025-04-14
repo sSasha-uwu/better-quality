d93407ca4c72039d5af33b0f39f9397187d599f2
sSasha <sshderm@gmail.com>
2025-04-15 00:20:17 +1000

    Fixed automatic underground placement and upgrade planner interactions

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -11,25 +11,24 @@ local new_items = {}
 
 for prototype_name, prototype_value in pairs(CHANGED_ENTITIES) do
     for entity_name, _ in pairs(prototype_value) do
-        log("base item:" .. entity_name)
         for quality_name, quality_value in pairs(qualities) do
             if quality_value.level > 0 then
                 local new_entity = table.deepcopy(data.raw[prototype_name][entity_name])
                 new_entity.localised_name = {"entity-name." .. new_entity.name}
                 new_entity.localised_description = {"entity-description." .. new_entity.name}
-                new_entity.hidden = true
+                new_entity.hidden = false
                 new_entity.name = "sSashaQuality-" .. quality_name .. "-" .. new_entity.name
                 new_entity.placeable_by = {item=new_entity.name, count=1, quality=quality_value}
                 new_entity.minable["result"] = new_entity.name
-                log(new_entity.name)
+                if prototype_name == "transport-belt" then
+                    new_entity.related_underground_belt = "sSashaQuality-" .. quality_name .. "-" .. new_entity.related_underground_belt
+                end
                 for stat_name, stat_value in pairs(CHANGED_ENTITIES[prototype_name][entity_name]) do
                     if IsDictionary(stat_value) then
                         for stat_table_name, stat_table_value in pairs(stat_value) do
-                            log(stat_table_name .. "|" .. stat_table_value[quality_value.level])
                             new_entity[stat_name][stat_table_name] = stat_table_value[quality_value.level]
                         end
                     else
-                        log(stat_name .. " | " .. stat_value[quality_value.level])
                         new_entity[stat_name] = stat_value[quality_value.level]
                     end
                 end
